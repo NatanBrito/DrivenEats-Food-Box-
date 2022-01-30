1c440d10c088b2e7b93c6a4de02e61c437cde972
Natan Ismael de Matos de Brito <natanismaelbrito@gmail.com>
2022-01-29 22:32:17 -0300

    colocado os nomes exigidos

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function selecionado() {
+function selecionado1() {
 const elemento= document.querySelector(".quadro-almoco");
-elemento.classList.add("select") ;  
+elemento.classList.toggle("select") ;  
 }
+function selecionado2() {
+    const elemento= document.querySelector(".quadro-almoco");
+    elemento.classList.add("select") ;  
+    }
+
+    function selecionado3() {
+        const elemento= document.querySelector(".quadro-almoco");
+        elemento.classList.add("select") ;  
+        }
